f93e5bc59ce7fdb752d4b656a0f5d885815c00e8
owzo <skye.stubbins@gmail.com>
2024-12-22 16:48:53 -0500

    Update install.lua
    Removed the /lib/ path prefix in the files table to align with the root structure of the repository.

--- install.lua
+++ install.lua
@@ -4,10 +4,10 @@
 local files = {
     "config.lua",
     "main_control.lua",
-    "lib/reac_utils.lua",
-    "lib/monitor_utils.lua",
-    "lib/stat_utils.lua",
-    "lib/energy_core_utils.lua"
+    "reac_utils.lua",
+    "monitor_utils.lua",
+    "stat_utils.lua",
+    "energy_core_utils.lua",
 }
 
 local baseUrl = "https://raw.githubusercontent.com/owzo/CC-Tweaked-Draconic-Manager/main/"
