8336d52025fff33c334ed8ba03389aa91f80c930
owzo <skye.stubbins@gmail.com>
2024-12-22 17:10:37 -0500

    Update stat_utils.lua

--- stat_utils.lua
+++ stat_utils.lua
@@ -10,27 +10,51 @@ Includes logging reactor statistics and generating reports.
 
 local config = require("config").energyCore
 
+-- Peripherals
+local energyCore
+
+-- Function to validate peripherals (Newly Added)
+local function validatePeripherals()
+    energyCore = peripheral.find("draconic_rf_storage")
+    if not energyCore then error("Energy Core not found!") end
+end
+
+-- Call to validate peripherals (Ensures peripherals are checked before usage)
+validatePeripherals()
+
 -- Function to log errors
 function logError(err)
     local logFile = fs.open(config.logsFile, "a")
-    logFile.writeLine(os.date() .. ": " .. err)
-    logFile.close()
+    if logFile then
+        logFile.writeLine(os.date() .. ": " .. err)
+        logFile.close()
+    else
+        print("Failed to open log file.")
+    end
 end
 
 -- Function to log reactor statistics
 function logReactorStats(reactor)
     local logFile = fs.open("reactor_stats.log", "a")
-    local info = reactor.getReactorInfo()
-    logFile.writeLine(os.date() .. ": " .. textutils.serialize(info))
-    logFile.close()
+    if logFile then
+        local info = reactor.getReactorInfo()
+        logFile.writeLine(os.date() .. ": " .. textutils.serialize(info))
+        logFile.close()
+    else
+        print("Failed to open reactor statistics log file.")
+    end
 end
 
 -- Function to log energy core statistics
-function logEnergyCoreStats(energyCore, inputRate, outputRate)
+function logEnergyCoreStats(inputRate, outputRate)
     local logFile = fs.open("energy_core_stats.log", "a")
-    local energyStored = energyCore.getEnergyStored()
-    local maxEnergyStored = energyCore.getMaxEnergyStored()
-    local transferRate = energyCore.getTransferPerTick()
-    logFile.writeLine(os.date() .. ": Stored=" .. energyStored .. ", Max=" .. maxEnergyStored .. ", Transfer=" .. transferRate .. ", InputRate=" .. inputRate .. ", OutputRate=" .. outputRate)
-    logFile.close()
+    if logFile then
+        local energyStored = energyCore.getEnergyStored()
+        local maxEnergyStored = energyCore.getMaxEnergyStored()
+        local transferRate = energyCore.getTransferPerTick()
+        logFile.writeLine(os.date() .. ": Stored=" .. energyStored .. ", Max=" .. maxEnergyStored .. ", Transfer=" .. transferRate .. ", InputRate=" .. inputRate .. ", OutputRate=" .. outputRate)
+        logFile.close()
+    else
+        print("Failed to open energy core statistics log file.")
+    end
 end
